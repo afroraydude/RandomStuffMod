5ff8228a57feb024c1991f065a9e44d0ff5aca86
afroraydude <rayjo5000@yahoo.com>
2014-09-14 16:11:51 -0400

    added some stuff to the biome

--- main/java/com/afroraydude/mods/randomstuff/biome/TestBiome.java
+++ main/java/com/afroraydude/mods/randomstuff/biome/TestBiome.java
@@ -1,5 +1,10 @@
 package com.afroraydude.mods.randomstuff.biome;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockGlowstone;
+import net.minecraft.entity.monster.EntitySlime;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.biome.BiomeDecorator;
 import net.minecraft.world.biome.BiomeGenBase;
 
 /**
@@ -9,5 +14,16 @@ public class TestBiome extends BiomeGenBase{
 
     public TestBiome(int id) {
         super(id);
+        this.theBiomeDecorator.treesPerChunk = 5;
+        this.theBiomeDecorator.bigMushroomsPerChunk = 5;
+        this.getSkyColorByTemp(1.9F);
+
+        // The top and filler block
+        this.topBlock = Blocks.grass;
+        this.fillerBlock = Blocks.clay;
+
+        // The entities that spawn and how many of said entities spawn in each group in this order: average, min, max
+        this.spawnableCreatureList.add(new SpawnListEntry(EntitySlime.class, 5, 1, 10));
+
     }
 }
